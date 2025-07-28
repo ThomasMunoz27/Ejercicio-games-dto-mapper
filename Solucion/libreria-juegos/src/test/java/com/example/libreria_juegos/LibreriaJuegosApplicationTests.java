41e3ac5f17ebbfbdc239d0bfc09d3893284b2b19
ThomasMunoz27 <thomasnm2004@gmail.com>
2025-07-28 08:14:34 -0300

    first commit

---
+++ Solucion/libreria-juegos/src/test/java/com/example/libreria_juegos/LibreriaJuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libreria_juegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaJuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
